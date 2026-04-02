13423930c9cc82992bd7fc66930a279dc80ed114
rob587 <robertocamm02@yahoo.com>
2026-04-02 14:25:25 +0200

    Fetch and display popular films from TMDB API with logging for debugging

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const API_KEY = process.env.EXPO_PUBLIC_TMDB_API_KEY;
+
+  const [film, setFilm] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=it-IT&page=1`,
+    )
+      .then((res) => res.json())
+      .then((data) => setFilm(data.results));
+  }, []);
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=it-IT&page=1`,
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       // 🔍 QUESTI console.log ti svelano TUTTA la struttura!
+  //       console.log("📦 RISPOSTA COMPLETA:", data);
+  //       console.log("🎬 ARRAY DEI FILM (results):", data.results);
+  //       console.log("🍿 PRIMO FILM:", data.results?.[0]);
+  //       console.log(
+  //         "📝 CHIAVI del primo film:",
+  //         data.results?.[0] ? Object.keys(data.results[0]) : "nessun film",
+  //       );
+
+  //       setFilm(data.results || []);
+  //     });
+  // }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.contenitore}>
+      <Text>ciao.</Text>
+      <FlatList
+        data={film}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.overview}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  contenitore: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
