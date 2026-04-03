ccd3554bf0e1d8c5958331eb78ce64cd2f088c1f
rob587 <robertocamm02@yahoo.com>
2026-04-04 00:01:05 +0200

    funzione di salvataggio in componente con persistenza di async/storage

--- app/(tabs)/preferiti.tsx
+++ app/(tabs)/preferiti.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "expo-router";
+import React, { useCallback, useState } from "react";
 import { Text, View } from "react-native";
 
+const IMAGE_BASE = "https://image.tmdb.org/t/p/w200";
+
 export default function preferiti() {
+  const [preferiti, setPreferiti] = useState([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadPreferiti;
+    }, []),
+  );
+
+  const loadPreferiti = async () => {
+    const filmSalvato = await AsyncStorage.getItem("preferiti");
+    if (filmSalvato) setPreferiti(JSON.parse(filmSalvato));
+  };
+
   return (
     <View>
       <Text>preferiti</Text>
